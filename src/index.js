479f57f46d7d91b765887e19b098dc867866a1b4
Matheusalves112 <113462888+Matheusalves112@users.noreply.github.com>
2024-09-25 14:34:49 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+// Importa a biblioteca React para criar componentes e gerenciar o estado da aplicação.
 import React from "react";
+
+// Importa ReactDOM, que é responsável por manipular a árvore de elementos DOM no navegador.
 import ReactDOM from "react-dom";
+
+// Importa o componente principal "App" da aplicação, que será renderizado.
 import App from "./App";
 
+// Usa o método render de ReactDOM para renderizar o componente <App /> dentro do elemento HTML com o ID "root".
+// O React.StrictMode é um wrapper para ajudar a detectar erros no código durante o desenvolvimento.
 ReactDOM.render(
+  // StrictMode ativa verificações adicionais e alertas no desenvolvimento
   <React.StrictMode>
+    {/* O componente principal da aplicação que será exibido */}
     <App />
   </React.StrictMode>,
+  
+  // Seleciona o elemento com ID "root" no HTML como ponto de montagem da aplicação React
   document.getElementById("root")
 );
