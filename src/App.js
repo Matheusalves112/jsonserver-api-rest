dc7b6c3ce06d6aafc0f1ae70d06842b7f2efb893
Matheusalves112 <113462888+Matheusalves112@users.noreply.github.com>
2024-09-25 14:33:07 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
+// Importa o componente LogoIcon do diretório "./assets/icons"
 import { LogoIcon } from "./assets/icons"
+
+// Importa o componente CrudUser do diretório "./components/CrudUser"
 import CrudUser from "./components/CrudUser"
+
+// Importa o arquivo de estilos CSS para o App
 import "./styles/App.css"
 
+// Função principal do componente App
 function App() {
-	return (
-		<>
-			<header>
-				<div className='header__content'>
-					<div className='logo'>
-						<LogoIcon />
-						<strong>JSON SERVER API</strong>
-					</div>
-				</div>
-			</header>
-			<main>
-				<CrudUser />
-			</main>
-		</>
-	)
+  return (
+    <>
+      {/* Definindo o cabeçalho da aplicação */}
+      <header>
+        <div className='header__content'>
+          {/* Seção da logo no cabeçalho */}
+          <div className='logo'>
+            {/* Componente LogoIcon que renderiza um ícone de logo */}
+            <LogoIcon />
+            {/* Título forte com o nome da aplicação */}
+            <strong>JSON SERVER API</strong>
+          </div>
+        </div>
+      </header>
+
+      {/* Corpo principal da aplicação */}
+      <main>
+        {/* Renderiza o componente CrudUser que provavelmente é responsável por operações de CRUD (Create, Read, Update, Delete) de usuários */}
+        <CrudUser />
+      </main>
+    </>
+  )
 }
 
+// Exporta o componente App como padrão para ser usado em outras partes da aplicação
 export default App
+
